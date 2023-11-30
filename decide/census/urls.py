3a585a90220b79b3132963b48bc51a73fe89e582
Practica <practica@pc-14-145.etsii>
2023-11-30 19:15:25 +0100

    dos

--- decide/census/urls.py
+++ decide/census/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from . import views
 
-
+#dos
 urlpatterns = [
     path('', views.CensusCreate.as_view(), name='census_create'),
     path('<int:voting_id>/', views.CensusDetail.as_view(), name='census_detail'),
